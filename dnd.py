02002b57bc74da8ebf1170f21c72e71e5929e917
WAFFLE_HAU5 <117693381+Waffle-Hau5@users.noreply.github.com>
2023-01-25 23:56:43 -0500

    made a "name generator" for monsters.

--- dnd.py
+++ dnd.py
@@ -1,9 +1,10 @@
-from random import randrange
+import random
 from playerdnd import PlayerDnd as p
 
 name = input("What is your name?")
-player = p(name, 200, 200, 1000, None)
-# these are latin names/prefixes that combine to create a name for the monsters.
+player = p(name=name, lvl=1, race=None, hp=100, attack=100, defense=100)
+
+# these are latin pre/suffixes that combine to create a name for the monsters encountered in the lambda function below.
 prefixes = prefixes = ["Aegi", "Aure", "Draco", "Eldar", "Fenr", "Goli", "Hydr", "Igni", "Jormung", "Krak",
             "Lup", "Medu", "Naga", "Ophi", "Pyr", "Quetz", "Rai", "Saur", "Taur", "Urs",
             "Vulc", "Wyrm", "Xiph", "Ym", "Zeph", "Aeg", "Aur", "Drac", "Eld", "Fen", "Gol",
@@ -12,28 +13,23 @@ prefixes = prefixes = ["Aegi", "Aure", "Draco", "Eldar", "Fenr", "Goli", "Hydr",
             "El", "Fe", "Go", "Hy", "Ig", "Jo", "Kr", "Lu", "Me", "Na", "Op", "Py", "Qu", "Ra",
             "Sa", "Ta", "Ur", "Vu", "Wy", "Xi", "Ym", "Ze"]
 
-suffixes = ["is", "lia", "n", "ion", "ir", "ath", "a", "s", "and", "en"]
+suffixes = ["is", "lia", "n", "ion", "ir", "ath", "a", "s", "and", "en", "on"]
+
+create_names = lambda : random.choice(prefixes) + random.choice(suffixes)
 
 def rollforbasestats():  
-    player.hp += randrange(1, 400)
-    player.attack += randrange(1, 400)
-    player.defense += randrange(1, 400)
+    player.hp += random.randint(1, 400)
+    player.attack += random.randint(1, 400)
+    player.defense += random.randint(1, 400)
 
 monsters = []
 
-def create_names():
-    # selects a random prefix and suffix from the prefixes and suffixes in monster name lists.
-    name = random.choice(prefixes) + random.choice(suffixes)
-    return name
-
-
-
 def create_monsters(lvl = p.lvl):
-    monstername = create_monster_names()
+    monstername = create_names()
     enemy = p(monstername, 50, 50, 1200, None)
-    enemy.hp += randrange(lvl*2, lvl*10)
-    enemy.attack += randrange(lvl*2, lvl*10)
-    enemy.defense += randrange(lvl*2, lvl*10)
+    enemy.hp += random.randint(lvl*10, lvl*20)
+    enemy.attack += random.randint(lvl*5, lvl*10)
+    enemy.defense += random.randint(lvl*5, lvl*10)
     monsters.append(enemy)
     return monsters
 
@@ -45,10 +41,6 @@ HP: {player.hp} ATK: {player.attack} DEF: {player.defense}""")
 
 p.races(self=player)
 
-p.increasestats(self=player, ihp=20, iatk=20, idef=20)  # self receives boost/buff
+p.increasestats(self=player, ihp=20, iatk=20, idef=20)  # testing method
 print("dict after p.increasestats:")
 print(player.__dict__)
-p.atk(self=player, atkr=enemy)  # self recieves damage, atkr is the obj for attacker.
-print("dict after being attacked:")
-print(player.__dict__)
-print(enemy.__dict__)
